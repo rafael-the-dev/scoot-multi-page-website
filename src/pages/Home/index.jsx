c26cf08ba097a5a70bf37c3bd593ec663eb7013a
Rafael Tivane <luislangabusiness@gmail.com>
2022-02-06 10:34:32 +0200

    adding desktop responsiveness to hero section

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,7 +2,7 @@ import classNames from 'classnames';
 import { Link } from 'react-router-dom';
 import { useStyles } from './styles'
 import { useGlobalStyles } from '../../styles';
-import { Button, Typography } from '@mui/material';
+import { Button, Hidden, Typography } from '@mui/material';
 
 const Home = () => {
     const classes = useStyles();
@@ -10,22 +10,23 @@ const Home = () => {
 
     return (
         <main>
-            <section className={classNames(globalStyles.px, classes.hero, `bg-no-repeat
-                flex flex-col justify-center items-center`)}>
+            <section className={classNames(globalStyles.px, classes.hero, `bg-no-repeat md:mb-24
+                flex flex-col justify-center items-center md:items-start`)}>
                 <Typography
-                    className={classNames('font-bold text-white text-center sm:text-5xl')}
+                    className={classNames('font-bold text-white text-center md:text-left sm:text-5xl')}
                     component="h1"
                     variant="h4">
-                    Scooter sharing made simple
+                    Scooter sharing<Hidden mdDown><br/></Hidden> made simple
                 </Typography>
                 <Typography
-                    className={classNames('mt-4 text-white text-center text-base')}
+                    className={classNames('mt-4 text-white text-center text-base md:text-left', 
+                    classes.heroDescription)}
                     variant="body2">
                         Scoot takes the hassle out of urban mobility. Our bikes are placed in 
                         convenient locations in each of our cities. Use our app to locate 
                         the nearest bike, unlock it with a tap, and you’re away!
                 </Typography>
-                <Link to="/">
+                <Link to="/" className={classNames(classes.heroLink)}>
                     <Button 
                         className={classNames('capitalize px-8 py-2 mt-6', globalStyles.yellowBg)}
                         variant="contained">
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -3,6 +3,8 @@ import hero from '../../assets/images/home-hero-mobile.jpg'
 import rightArrow from '../../assets/images/patterns/right-arrow.svg'
 import heroTablet from '../../assets/images/home-hero-tablet.jpg'
 import whiteCircles from '../../assets/images/patterns/white-circles.svg'
+import heroDesktop from '../../assets/images/home-hero-tablet.jpg'
+import leftDownwardArrow from '../../assets/images/patterns/left-downward-arrow.svg'
 
 const linearGradient = 'linear-gradient(to bottom, rgba(0, 0, 0, .4), rgba(0, 0, 0, .1))';
 
@@ -17,6 +19,23 @@ export const useStyles = makeStyles(theme => ({
             backgroundPosition: 'center, left -118px bottom 18px, right -24px bottom 39px, center',
             backgroundSize: 'cover, 354px, 175px, cover',
             height: 550,
+        },
+        [theme.breakpoints.up('md')]: {
+            backgroundImage: `url(${leftDownwardArrow}), url(${rightArrow}), url(${whiteCircles}), ${linearGradient}, url(${heroDesktop})`,
+            backgroundPosition: 'left -452px bottom 130px, left 600px bottom 127px, right -24px bottom 132px, center, center',
+            backgroundSize: '531px, 354px, 175px, cover, cover',
+            height: 500,
+        }
+    },
+    heroDescription: {
+        [theme.breakpoints.up(1200)]: {
+            marginLeft: 80,
+            width: 400
+        }
+    },
+    heroLink: {
+        [theme.breakpoints.up(1200)]: {
+            marginLeft: 80,
         }
     }
 }));
