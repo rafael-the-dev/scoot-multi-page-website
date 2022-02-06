b738dac4b33041e0bca8cfa59f4a7958b8bd7ea1
Rafael Tivane <luislangabusiness@gmail.com>
2022-02-06 09:38:25 +0200

    adding background pattern to banner

--- src/components/SignInBanner/index.jsx
+++ src/components/SignInBanner/index.jsx
@@ -3,7 +3,7 @@ import { useStyles } from './styles'
 import { useGlobalStyles } from '../../styles';
 import appStore from '../../assets/images/icons/app-store.svg'
 import googlePlay from '../../assets/images/icons/google-play.svg'
-import { Typography } from '@mui/material';
+import { Hidden, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 const SignInBanner = () => {
@@ -12,11 +12,14 @@ const SignInBanner = () => {
 
     return (
         <section className={classNames(globalStyles.px, globalStyles.darkNavyBg, `py-12 bg-no-repeat 
-            flex items-center flex-col`)}>
-            <Typography component="h2" variant="h4" className={classNames('text-white text-center font-bold')}>
-                Sign up and Scoot off today
+            flex items-center flex-col md:flex-row md:justify-between sm:py-16`, classes.signInBanner)}>
+            <Typography 
+                component="h2" 
+                variant="h4" 
+                className={classNames('text-white text-center font-bold md:text-left')}>
+                Sign up and<Hidden mdDown><br/></Hidden> Scoot off today
             </Typography>
-            <div className={classNames('mt-8 flex items-center')} >
+            <div className={classNames('mt-8 md:mt-0 flex items-center')} >
                 <Link 
                      className={classNames(classes.imageContainer, 'mr-4')} 
                     to="/">
--- src/components/SignInBanner/styles.js
+++ src/components/SignInBanner/styles.js
@@ -1,6 +1,17 @@
 import { makeStyles } from "@mui/styles";
+import semiCircles from '../../assets/images/patterns/semi-circles.svg'
 
 export const useStyles = makeStyles(theme => ({
+    signInBanner: {
+        backgroundImage: `url(${semiCircles})`,
+        backgroundPosition: 'left -605px bottom',
+        [theme.breakpoints.up('sm')]: {
+            backgroundPosition: 'right bottom',
+        },
+        [theme.breakpoints.up('md')]: {
+            backgroundPosition: 'right -200px bottom',
+        }
+    },
     imageContainer: {
         height: 40,
         width: 120
