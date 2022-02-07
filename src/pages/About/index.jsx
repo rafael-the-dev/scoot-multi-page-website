89bbfcbc2e579fd8928ddc0fbc8af0b7ba638219
Rafael Tivane <luislangabusiness@gmail.com>
2022-02-07 17:46:22 +0200

    adding new cards

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -4,6 +4,9 @@ import { useStyles } from './styles'
 import { useGlobalStyles } from '../../styles';
 import { Button, Hidden, Typography } from '@mui/material';
 import DefaultHero from '../../components/DefaultHero';
+import DefaultCard from '../../components/DefaultCard';
+import digitalEraImage from '../../assets/images/digital-era.jpg'
+import betterLivingImage from '../../assets/images/better-living.jpg'
 
 const AboutPage = () => {
     const classes = useStyles();
@@ -15,6 +18,20 @@ const AboutPage = () => {
                 page="about"
                 title='About'
             />
+            <div className={classNames('pt-24 md:pt-20')}>
+                <DefaultCard 
+                    description="Getting around should be simple (and even fun!) for everyone. We embrace technology to provide low cost, smart access to scooters at your fingertips."
+                    image={digitalEraImage}
+                    index={0}
+                    title="Mobility for the digital era"
+                />
+                <DefaultCard 
+                    description="We’re helping connect cities and bring people closer together. Our scooters are also fully-electric and we offset the minimal carbon footprint for each ride."
+                    image={betterLivingImage}
+                    index={1}
+                    title="Better urban living"
+                />
+            </div>
         </main>
     );
 };
