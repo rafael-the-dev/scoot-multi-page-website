fa960a32c380789210fc9ba6c47f8ce4f6a54bb8
Rafael Tivane <luislangabusiness@gmail.com>
2022-02-07 08:48:25 +0200

    adding background pattern images to cards images

--- src/components/DefaultCard/index.jsx
+++ src/components/DefaultCard/index.jsx
@@ -5,21 +5,30 @@ import { useStyles } from './styles'
 import { useGlobalStyles } from '../../styles';
 import { Card, CardContent, CardMedia, Typography } from '@mui/material';
 
-const DefaultCard = ({ description, image, title }) => {
+const DefaultCard = ({ description, image, index, title }) => {
     const classes = useStyles();
     const globalStyles = useGlobalStyles();
+
+    const imagesClasses = useMemo(() => [
+        classes.telemetry,
+        classes.nearYou, 
+        classes.payments
+    ], [ classes ])
 
     return (
         <Card 
             elevation={0} 
-            className={classNames(globalStyles.px, 'mb-8')}
+            className={classNames(globalStyles.px, classes.card, `mb-8`)}
             component="article">
-            <CardMedia
-                alt={ title }
-                className={classNames(classes.cardMedia, 'rounded-full')}
-                component="img"
-                image={ image }
-            />
+            <div className={classNames(classes.cardMediaContainer, imagesClasses[index], `relative after:block after:w-full after:h-full
+                after:absolute after:bg-no-repeat `)}>
+                <CardMedia
+                    alt={ title }
+                    className={classNames(classes.cardMedia, 'rounded-full ')}
+                    component="img"
+                    image={ image }
+                />
+            </div>
             <CardContent className={classNames('pt-8')}>
                 <Typography 
                     className={classNames(globalStyles.darkNavyColor, 'text-center font-bold')}
--- src/components/DefaultCard/styles.js
+++ src/components/DefaultCard/styles.js
@@ -1,7 +1,36 @@
 import { makeStyles } from "@mui/styles";
+import rightArrow from '../../assets/images/patterns/right-arrow.svg'
+import leftDownwardArrow from '../../assets/images/patterns/left-downward-arrow.svg'
 
 export const useStyles = makeStyles(theme => ({
-   footer: {
-       backgroundColor: '#333A44'
-   }
+    card: {
+        
+    },
+    cardMediaContainer: {
+        '&::after': {
+            backgroundImage: `url(${leftDownwardArrow})`,
+            backgroundSize: 597,
+            content: '""',
+            left: 0,
+            top: 0
+        }
+    },
+    telemetry: {
+        '&::after': {
+            backgroundPosition: 'right -293px bottom 12px'
+        }
+    },
+    nearYou: {
+        '&::after': {
+            backgroundImage: `url(${rightArrow})`,
+            backgroundPosition: 'left -221px top',
+            backgroundSize: 433,
+        }
+    },
+    payments: {
+        '&::after': {
+            backgroundPosition: 'right -475px top 2px',
+            backgroundSize: 673,
+        }
+    }
 }));
