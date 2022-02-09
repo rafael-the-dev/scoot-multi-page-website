d498945de3b5ab93a9d6affb4e311ba6e742fd27
Rafael Tivane <luislangabusiness@gmail.com>
2022-02-09 09:06:11 +0200

    finishing not-listed locations responsiveness

--- src/pages/Locations/index.jsx
+++ src/pages/Locations/index.jsx
@@ -11,21 +11,24 @@ const Locations = () => {
 
     return (
         <main>
-            <section className={classNames(globalStyles.px, `flex flex-col items-center py-12`)}>
+            <section className={classNames(globalStyles.px, `flex flex-col items-center py-12 md:flex-row
+                md:justify-between`)}>
                 <Typography 
-                    className={classNames(globalStyles.darkNavyColor, 'text-center md:text-left font-bold')}
+                    className={classNames(globalStyles.darkNavyColor, classes.notListedLocationTitle, `text-center md:text-left font-bold 
+                    md:text-4xl md:w-52`)}
                     component="h2"
                     variant="h4" 
                     >
                     Your City Not Listed?
                 </Typography>
                 <Typography 
-                    className={classNames(globalStyles.dimGreyColor, 'mt-6 text-center md:text-left sm:text-base')}
+                    className={classNames(globalStyles.dimGreyColor, classes.notListedLocationsDescription, 
+                        `mt-6 md:mt-0 text-center md:text-left sm:text-base md:growhh`)}
                     variant="body2" 
                     >
                     If you’d like to see Scoot in your hometown, be sure to let us know. We track requests and plan launches based on demand. Feel free to message us by clicking the link or messaging us on social.
                 </Typography>
-                <Link to="/" className={classNames(classes.heroLink, ' mt-8')}>
+                <Link to="/" className={classNames(classes.heroLink, ' mt-8 md:mt-0')}>
                     <Button 
                         className={classNames('capitalize px-8 py-2', globalStyles.yellowBg)}
                         variant="contained">
--- src/pages/Locations/styles.js
+++ src/pages/Locations/styles.js
@@ -1,5 +1,12 @@
 import { makeStyles } from "@mui/styles";
 
 export const useStyles = makeStyles(theme => ({
-    
+    notListedLocationsDescription: {
+        [theme.breakpoints.up('md')]: {
+            maxWidth: '50%'
+        },
+        [theme.breakpoints.up(1200)]: {
+            maxWidth: '60%'
+        }
+    }
 }));
