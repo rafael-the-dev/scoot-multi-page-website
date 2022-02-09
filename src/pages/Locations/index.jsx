3cfa6e54c7a8019e8d232bf1a078b16565ae2956
Rafael Tivane <luislangabusiness@gmail.com>
2022-02-09 08:53:02 +0200

    creating not-listed locations section

--- src/pages/Locations/index.jsx
+++ src/pages/Locations/index.jsx
@@ -1,7 +1,8 @@
 import classNames from 'classnames'
 import { useStyles } from './styles'
 import { useGlobalStyles } from '../../styles';
-import { Typography } from '@mui/material';
+import { Button, Typography } from '@mui/material';
+import { Link } from 'react-router-dom'
 import ourCommunityImage from '../../assets/images/our-community.jpg'
 
 const Locations = () => {
@@ -10,25 +11,25 @@ const Locations = () => {
 
     return (
         <main>
-            <section>
+            <section className={classNames(globalStyles.px, `flex flex-col items-center py-12`)}>
                 <Typography 
                     className={classNames(globalStyles.darkNavyColor, 'text-center md:text-left font-bold')}
                     component="h2"
-                    variant="h5" 
+                    variant="h4" 
                     >
-                    { title }
+                    Your City Not Listed?
                 </Typography>
                 <Typography 
                     className={classNames(globalStyles.dimGreyColor, 'mt-6 text-center md:text-left sm:text-base')}
                     variant="body2" 
                     >
-                    { description }
+                    If you’d like to see Scoot in your hometown, be sure to let us know. We track requests and plan launches based on demand. Feel free to message us by clicking the link or messaging us on social.
                 </Typography>
                 <Link to="/" className={classNames(classes.heroLink, ' mt-8')}>
                     <Button 
                         className={classNames('capitalize px-8 py-2', globalStyles.yellowBg)}
                         variant="contained">
-                        Learn more
+                        Message us
                     </Button>
                 </Link>
             </section>
